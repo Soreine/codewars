a8cfc90100c940f7c09ae99e4a2653d86d372042
Soreine <soreine.plume@gmail.com>
2015-08-02 23:40:47 +0200

    Removed reduce and map so we don't have Out Of Memory errors. Complexity O(n2)...

--- longest-2-characters-substring.js
+++ longest-2-characters-substring.js
@@ -1,21 +1,24 @@
 // return the longest substring within str which contains at most 2 characters.
+// Complexity O(n*k) with n the string length and k the length of the result
 function substring(str) {
-  var chars = str.split('');
-
+  var chars = str.split(''); 
+  var iter = 0;
+ 
   // Find the longest series of 2 different characters (c1 or c2)
-  // starting at chars[index] of length max
-  function longest2(chars, index, max, c1, c2,) {
-    if(index >= chars.length || max === 0) {
+  // starting at chars[index]
+  function longest2(chars, index, c1, c2) {
+    if(index >= chars.length) {
       return "";
     } else {
+      iter++;
       var c = chars[index];
       if (c1 === undefined) {
-        return c + longest2(chars, index+1, max-1, c);
+        return c + longest2(chars, index+1, c);
       } else if (c2 === undefined) {
         if(c !== c1) {
           c2 = c;
         }
-        return c + longest2(chars, index+1, max-1, c1, c2);
+        return c + longest2(chars, index+1, c1, c2);
       } else {
         if(c === c1 || c === c2) {
           return c + longest2(chars, index+1, c1, c2);
@@ -26,8 +29,18 @@ function substring(str) {
     }
   }
 
-  return chars.map(function(char, index) { return longest2(chars, index); })
-    .reduce(function (acc, s) { return s.length > acc.length ? s : acc; }, "");
+  var maxLength = 0;
+  var maxString = "";
+  var s;
+  for(var i = 0; i < str.length - maxLength; ++i) {
+    s = longest2(chars, i);
+    if(s.length > maxLength) {
+      maxLength = s.length;
+      maxString = s;
+    }
+  }
+  console.log(str.length + " : " + iter);
+  return maxString;
 }
 
 console.log(substring("") + ' => ""');
@@ -36,3 +49,6 @@ console.log(substring("aaa") + ' => "aaa"');
 console.log(substring("abacd") + ' => "aba"');
 console.log(substring("abacddcd") + ' => "cddcd"');
 console.log(substring("cefageaacceaccacca") + ' => "accacca"');
+console.log(substring("abcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabc") + ' => "ab"');
+// This is kind of worst case... O(n²)
+console.log(substring("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaacbaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa") + ' => "aaaaac"');
